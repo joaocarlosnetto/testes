cca66b2651ba0fc25d5ff52c9d2f004e8acdb245
joão carlos de caravlho <140458265+joaocarlosnetto@users.noreply.github.com>
2024-05-29 20:18:00 -0300

    editei meu proeto

---
+++ projeto/dior.js
@@ -0,0 +1,29 @@
+function seta(){
+    var bases = document.getElementById('bases')
+    var lights = document.getElementById('lights')
+    if(bases.style.display == 'none'){
+        bases.style.display = 'block'
+        lights.style.display = 'none'
+    } else{
+        bases.style.display = 'none'
+        lights.style.display = 'block'
+
+    }
+    bases.style.transform = '0.3s'
+    lights.style.transition = '0.3s'
+}
+
+function back(){
+    var bases = document.getElementById('bases')
+    var lights = document.getElementById('lights')
+    if(bases.style.display == 'none'){
+        bases.style.display = 'block'
+        lights.style.display = 'none'
+    } else{
+        bases.style.display = 'none'
+        lights.style.display = 'block'
+
+    }
+    bases.style.transform = '0.3s'
+    lights.style.transition = '0.3s'
+}
