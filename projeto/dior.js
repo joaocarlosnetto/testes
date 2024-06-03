ae42ecf7d7654bfcb3ccebc8fe6c83117e79a19e
joão carlos de caravlho <140458265+joaocarlosnetto@users.noreply.github.com>
2024-06-03 20:03:35 -0300

    editei meu projeto

--- projeto/dior.js
+++ projeto/dior.js
@@ -1,30 +1,23 @@
-function seta(){
-    var bases = document.getElementById('bases')
+function back(){
     var lights = document.getElementById('lights')
-    if(bases.style.display == 'none'){
-        bases.style.display = 'block'
-        lights.style.display = 'none'
-    } else{
-        bases.style.display = 'none'
+    var bases = document.getElementById('bases')
+    if(lights.style.display == 'none'){
         lights.style.display = 'block'
-
+        bases.style.display = 'none'
+    } else{
+        lights.style.display = 'none'
+        bases.style.display = 'block'
     }
-    bases.style.transform = '0.3s'
-    lights.style.transition = '0.3s'
-
 }
 
-function back(){
-    var bases = document.getElementById('bases')
+function seta(){
     var lights = document.getElementById('lights')
-    if(bases.style.display == 'none'){
-        bases.style.display = 'block'
-        lights.style.display = 'none'
-    } else{
-        bases.style.display = 'none'
+    var bases = document.getElementById('bases')
+    if(lights.style.display == 'none'){
         lights.style.display = 'block'
-
+        bases.style.display = 'none'
+    } else{
+        lights.style.display = 'none'
+        bases.style.display = 'block'
     }
-    bases.style.transform = '0.3s'
-    lights.style.transition = '0.3s'
 }
